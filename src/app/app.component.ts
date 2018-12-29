f5d11a3393e30485ad402f418ff2e1b85cd61de0
dasitha@sp-solutions.biz <dasitha@sp-solutions.biz>
2018-12-29 23:57:20 +0530

    Base convertor for decimal..octa,binary,hexa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {FormGroup , FormControl} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'BaseConverter';
+  form;
+  newValuew: any;
+  ngOnInit () {
+    this.form = new FormGroup({
+      decimal: new FormControl(''),
+      binary: new FormControl(''),
+      octal: new FormControl(''),
+      hexa: new FormControl(''),
+    });
+  }
+  decimalChanged = function (oldValue, newValue) {
+    if(newValue != '') {
+      this.form.patchValue({binary: parseInt(newValue, 10).toString(2)});
+      this.form.patchValue({octal: parseInt(newValue, 10).toString(8)});
+      this.form.patchValue({hexa: parseInt(newValue, 10).toString(16)});
+
+    }
+
+      else {this.form.patchValue({binary: ""});
+      this.form.patchValue({octal: ""});
+      this.form.patchValue({hexa: ""});}
+
+  };
+
 }
